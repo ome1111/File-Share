341b1191864c2bf0990de050430b4396dc60698d
ome1111 <61970570+ome1111@users.noreply.github.com>
2026-03-07 17:02:22 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,6 @@
-DB_URI = "mongodb+srv://boxleyglenn7:dQhnAobkUbdpJGPM@cluster0.wxp1pxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+import os
+
+# আপনার সার্ভারের (Render/Heroku) Environment Variables থেকে ডাটাবেসের লিংক নেবে
+DB_URI = os.environ.get("DATABASE_URL", "")
 JOIN_REQS_DB = DB_URI
-DB_NAME = "fair"
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
